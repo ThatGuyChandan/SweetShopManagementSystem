608b2d00274f84246167f471b8c544bc6b4ac447
Chandan Thakur <contact.chandanthakur@gmail.com>
2025-09-19 17:53:43 +0530

    basic setup

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const expres = require("express");
+const app = expres();
+const port = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
